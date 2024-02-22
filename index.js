049080878de2de70438e5b08ae561cd07c1db83f
nickth93 <41366878+nickth93@users.noreply.github.com>
2024-02-22 14:03:57 +0000

    commit 3

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-import
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var app = (0, express_1.default)();
+var port = 3000;
+app.get('/api', function (req, res) {
+    res.send('Hello, world!');
+});
+app.listen(port, function () {
+    console.log("server started at localhost:".concat(port));
+});
---
+++ index.ts
@@ -0,0 +1,51 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+const multer = require("multer");
+
+const handleError = (err, res) => {
+  res
+    .status(500)
+    .contentType("text/plain")
+    .end("Oops! Something went wrong!");
+};
+
+const upload = multer({
+  dest: "images"
+  
+});
+
+
+app.post(
+  "/upload",
+  upload.single("file" /* name attribute of <file> element in your form */),
+  (req, res) => {
+    const tempPath = req.file.path;
+    const targetPath = path.join(__dirname, "./uploads/image.png");
+
+    if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+      fs.rename(tempPath, targetPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(200)
+          .contentType("text/plain")
+          .end("File uploaded!");
+      });
+    } else {
+      fs.unlink(tempPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(403)
+          .contentType("text/plain")
+          .end("Only .png files are allowed!");
+      });
+    }
+  }
+
+
+app.listen(port, ()=> {
+    console.log(`server started at localhost:${port}`)
+});
